c1ad5aa43a9221da82bd6a25ee84eabefdd8342c
sujay pradhan <135443280+sujay090@users.noreply.github.com>
2025-09-15 13:38:46 +0530

    fix the endpoints problem

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_SERVER_URL|| 'http://localhost:3001/api';
 
 // Create axios instance
 const api = axios.create({
